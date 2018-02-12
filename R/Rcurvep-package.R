b4c227370721524e9b675f5ff11f610d0c378f6c
moggces <juihua.hsieh@gmail.com>
2018-02-12 16:23:40 -0500

    match package description to readme

--- R/Rcurvep-package.R
+++ R/Rcurvep-package.R
@@ -1,16 +1,19 @@
-#' Rcurvep: a R package for concentration response modeling using Curvep
+#' Rcurvep: an R package for concentration response modeling using Curvep
 #'
 #' The packages provides an R interface for processing concentration response data using Curvep, a response noise filtering algorithm.
 #'
-#' With a predefined response noise threshold in an experiment, it allows to calculate activity based on original or simulated concentration response data.
-#' The simulation process differs for three types of dataset:
+#' With a predefined baseline noise threshold (or minimum response threshold) in an experiment, it allows to calculate activity (with confidence interval) based on original or simulated concentration response data.
+#'
+#' If the baseline noise threshold is unknown, the above process can be repeated using a reasonable number of candidates.
+#' The optimal threshold is identified as the one that variance of potency estimation is sufficiently reduced and even stabilized.
+#'
+#' Currently simulated data can be generated from one of the three types of dataset:
+#'
 #' \itemize{
-#'   \item binary incidece data (e.g., mortality data)
-#'   \item high number of replicates (e.g., alternative animal model data)
-#'   \item low number of replicates (e.g., in vitro data)
+#'   \item dichotomous binary incidence data (e.g., mortality data from rodent model)
+#'   \item continuous data with high number of replicates (e.g., alternative animal model data)
+#'   \item continous data with low number of replicates (e.g., in vitro data)
 #' }
-#' If the response noise threshold is unknown, the above process can be repeated using a number of candidate thresholds.
-#' The optimal threshold is identified as the one that varience of potency estimation is sufficiently reduced and even stabilized.
 #'
 #' To learn more about Rcurvep, start with the vignettes:
 #' `browseVignettes(package = "Rcurvep")`
