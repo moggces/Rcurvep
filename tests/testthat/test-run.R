8a23f8f53d55a2cda4ff00e1ec8082d78a3c2955
moggces <juihua.hsieh@gmail.com>
2019-08-10 20:44:01 +0800

    add run_rcurvep

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -1,90 +1,112 @@
 context("running job")
 
+data("zfishbeh")
+dats <- zfishbeh %>%
+  dplyr::group_by(endpoint, chemical, conc) %>%
+  dplyr::slice(1) %>%
+  dplyr::ungroup() %>%
+  split(.$endpoint)
 
-test_that("percent data with n_sample = NULL", {
-  data("zfishdev")
-  x <- zfishdev %>%
-    split(.$endpoint)
-  outd <- run_curvep_batch(x[[1]],
-                         directionality = 1,
-                         n_sample = NULL,
-                         threshold = 15,
-                         other_paras = list(CARR = 20, TrustHi = TRUE))
-  expect_true(sum(colnames(outd) %in% c('input', 'output', 'activity')) == 3, info = "a failed run" )
+test_that("run one resp per conc per endpoint", {
+
+  outp <- run_rcurvep(dats[[1]], config = curvep_defaults())
+  expect_length(outp, 2)
+
 })
 
-
-test_that("resp data with n_sample = NULL", {
-  data("zfishbeh")
-  x <- zfishbeh %>%
-    split(.$endpoint)
-  outd <- run_curvep_batch(x[[1]],
-                         directionality = 1,
-                         n_sample = NULL,
-                         threshold = 15,
-                         other_paras = list(CARR = 20, TrustHi = TRUE))
-  expect_true(sum(colnames(outd) %in% c('input', 'output', 'activity')) == 3, info = "a failed run" )
+test_that("run one resp per conc many endpoints", {
+  outp <- run_rcurvep(dplyr::bind_rows(dats), config = curvep_defaults())
+  expect_length(outp, 2)
 })
 
+test_that("with parameter change", {
 
-test_that("resp data original with multiple thresholds", {
-  data("zfishbeh")
-  x <- zfishbeh %>%
-    split(.$endpoint)
-  outd <- run_curvep_batch(x[[1]],
-                         directionality = 0,
-                         n_sample = NULL,
-                         threshold = list("1" = c(15,20), "-1" = c(30)),
-                         other_paras = list(CARR = 20, TrustHi = TRUE))
-
-  outd <- outd %>% dplyr::select(threshold, direction) %>% dplyr::distinct() %>%
-    dplyr::arrange(direction, threshold) %>% dplyr::pull(threshold)
-  expect_equal(outd, c(30, 15, 20))
 })
 
-
-test_that("percent boot dataset job run", {
-  data("zfishdev")
-  x <- zfishdev %>%
-    split(.$endpoint)
-  outd <- run_curvep_batch(x[[1]],
-                         directionality = 1,
-                         n_sample = 2,
-                         threshold = seq(5, 10, by = 5),
-                         other_paras = list(CARR = 20, TrustHi = TRUE))
-
-  outd <- outd %>% dplyr::select(threshold, direction, repeat_id) %>% dplyr::distinct()
-
-  expect_true(nrow(outd) == 4, info = "a failed run" )
-})
-
-
-test_that("resp boot dataset job run", {
-  data("zfishbeh")
-  x <- zfishbeh %>%
-    split(.$endpoint)
-  outd <- run_curvep_batch(x[[1]],
-                         directionality = 1,
-                         n_sample = 2,
-                         threshold = seq(5, 10, by = 5),
-                         other_paras = list(CARR = 20, TrustHi = TRUE))
-
-  outd <- outd %>% dplyr::select(threshold, direction, repeat_id) %>% dplyr::distinct()
-  expect_true(nrow(outd) == 4, info = "a failed run" )})
-
-
-test_that("resp boot error dataset job run", {
-
-  error <- rnorm(100, 0, 10)
-  data("zfishbeh")
-  x <- zfishbeh %>%
-    split(.$endpoint)
-  outd <- run_curvep_batch(x[[1]],
-                         directionality = 1,
-                         n_sample = 2,
-                         threshold = seq(5, 10, by = 5),
-                         other_paras = list(CARR = 20, TrustHi = TRUE), vehicle_data = error )
-
-  outd <- outd %>% dplyr::select(threshold, direction, repeat_id) %>% dplyr::distinct()
-  expect_true(nrow(outd) == 4, info = "a failed run" )
-})
+# test_that("percent data with n_sample = NULL", {
+#   data("zfishdev")
+#   x <- zfishdev %>%
+#     split(.$endpoint)
+#   outd <- run_curvep_batch(x[[1]],
+#                          directionality = 1,
+#                          n_sample = NULL,
+#                          threshold = 15,
+#                          other_paras = list(CARR = 20, TrustHi = TRUE))
+#   expect_true(sum(colnames(outd) %in% c('input', 'output', 'activity')) == 3, info = "a failed run" )
+# })
+#
+#
+# test_that("resp data with n_sample = NULL", {
+#   data("zfishbeh")
+#   x <- zfishbeh %>%
+#     split(.$endpoint)
+#   outd <- run_curvep_batch(x[[1]],
+#                          directionality = 1,
+#                          n_sample = NULL,
+#                          threshold = 15,
+#                          other_paras = list(CARR = 20, TrustHi = TRUE))
+#   expect_true(sum(colnames(outd) %in% c('input', 'output', 'activity')) == 3, info = "a failed run" )
+# })
+#
+#
+# test_that("resp data original with multiple thresholds", {
+#   data("zfishbeh")
+#   x <- zfishbeh %>%
+#     split(.$endpoint)
+#   outd <- run_curvep_batch(x[[1]],
+#                          directionality = 0,
+#                          n_sample = NULL,
+#                          threshold = list("1" = c(15,20), "-1" = c(30)),
+#                          other_paras = list(CARR = 20, TrustHi = TRUE))
+#
+#   outd <- outd %>% dplyr::select(threshold, direction) %>% dplyr::distinct() %>%
+#     dplyr::arrange(direction, threshold) %>% dplyr::pull(threshold)
+#   expect_equal(outd, c(30, 15, 20))
+# })
+#
+#
+# test_that("percent boot dataset job run", {
+#   data("zfishdev")
+#   x <- zfishdev %>%
+#     split(.$endpoint)
+#   outd <- run_curvep_batch(x[[1]],
+#                          directionality = 1,
+#                          n_sample = 2,
+#                          threshold = seq(5, 10, by = 5),
+#                          other_paras = list(CARR = 20, TrustHi = TRUE))
+#
+#   outd <- outd %>% dplyr::select(threshold, direction, repeat_id) %>% dplyr::distinct()
+#
+#   expect_true(nrow(outd) == 4, info = "a failed run" )
+# })
+#
+#
+# test_that("resp boot dataset job run", {
+#   data("zfishbeh")
+#   x <- zfishbeh %>%
+#     split(.$endpoint)
+#   outd <- run_curvep_batch(x[[1]],
+#                          directionality = 1,
+#                          n_sample = 2,
+#                          threshold = seq(5, 10, by = 5),
+#                          other_paras = list(CARR = 20, TrustHi = TRUE))
+#
+#   outd <- outd %>% dplyr::select(threshold, direction, repeat_id) %>% dplyr::distinct()
+#   expect_true(nrow(outd) == 4, info = "a failed run" )})
+#
+#
+# test_that("resp boot error dataset job run", {
+#
+#   error <- rnorm(100, 0, 10)
+#   data("zfishbeh")
+#   x <- zfishbeh %>%
+#     split(.$endpoint)
+#   outd <- run_curvep_batch(x[[1]],
+#                          directionality = 1,
+#                          n_sample = 2,
+#                          threshold = seq(5, 10, by = 5),
+#                          other_paras = list(CARR = 20, TrustHi = TRUE), vehicle_data = error )
+#
+#   outd <- outd %>% dplyr::select(threshold, direction, repeat_id) %>% dplyr::distinct()
+#   expect_true(nrow(outd) == 4, info = "a failed run" )
+# })
