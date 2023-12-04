5391f1aeba7cebb9cc66cc6ad7f6859ed1e8967c
Hsieh <hsiehj2@nih.gov>
2023-12-04 11:14:41 -0500

    handle the no fit cases to return the default directly

--- R/curveclass2_base.R
+++ R/curveclass2_base.R
@@ -18,22 +18,55 @@
 #'
 fit_cc2_modl <- function(Conc, Resp, classSD = 5, minYrange = 20) {
 
-  # create java object and call
-  cf <- rJava::.jnew('gov/nih/ncats/ifx/qhts/curvefitting/CurveClassFit3')
   # the input in M
   Conc <- 10^Conc
-
   conc <- na.omit(Conc)
   resp <- na.omit(Resp)
 
-  if(length(conc) != length(resp)) rlang::abort("The length of conc and resp is not the same after removing NA.")
+  # default values
+  cc2_fit <- 0L
+  cc2_cc2 <- 4
+  cc2_aic <- NA_real_
+  cc2_tp <- NA_real_
+  cc2_ga <- NA_real_
+  cc2_gw <- NA_real_
+  cc2_bt <- NA_real_
+  cc2_pvalue <- NA_real_
+  cc2_r2 <- NA_real_
+  cc2_masks <- NA_character_
+  cc2_nmasks <- NA_integer_
 
   ## handle the cases of all NA ##
   ## special treatment ##
-  if (length(resp) == 0) {
-    conc <- 0
-    resp <- 0
+  if (length(resp) <= 1) { # it has to be an array to fullfill java signature
+    return(
+      list(
+        # consistency to tcpl
+        aic = cc2_aic,
+        fit = cc2_fit,
+        modl = "cc2",
+
+        # original output
+        cc2 = cc2_cc2,
+        gw = cc2_gw,
+        tp = cc2_tp,
+        bt = cc2_bt,
+        ga = cc2_ga,
+        masks = cc2_masks,
+        nMasks = cc2_nmasks,
+        pvalue = cc2_pvalue,
+        r2 = cc2_r2
+      )
+    )
   }
+
+  # handle (this has be checked in the input)
+  if(length(conc) != length(resp)) {
+    rlang::abort("The length of conc and resp is not the same after removing NA.")
+  }
+
+  # create java object and call
+  cf <- rJava::.jnew('gov/nih/ncats/ifx/qhts/curvefitting/CurveClassFit3')
 
   # call the curveclass2
   s <- rJava::.jcall(cf, returnSig = "S", method = "performCurveFitting",
@@ -45,41 +78,34 @@ fit_cc2_modl <- function(Conc, Resp, classSD = 5, minYrange = 20) {
   #  'maxResponse', 'pValue', 'r2', 'zeroActivity')
 
   # split the output into vector
-  # ac50 and maxResponse are not included in the list
+  # ac50 and maxResponse and efficacy are not included in the list
   v <- stringr::str_split_fixed(s, pattern = "\n", n = 13)[1:12]
 
-  # handle consistency
-  aic <- NA_real_
-  fit <- 1
-  suppressWarnings(hillcoef <- as.numeric(v[4])) #null to NA
-  if(is.na(hillcoef)) fit <- 0
+  # get hill coef first
+  suppressWarnings( #null to NA
+    hillcoef <- as.numeric(v[4])
+  )
+  # NULL cannot be returned in the ifelse
+  if(!is.na(hillcoef)) cc2_masks <- v[7]
 
-  # handle cc2 = 4 (no fit)
-  masks <- v[7] #null to "0 0 0 0"
-  nmasks <- v[8] #NA to 0
-  if (v[7] == "null") {
-    masks <- stringr::str_c(rep(0, length(conc)), collapse = " ")
-    nmasks <- 0
-  }
 
   return(
     suppressWarnings( #null to NA
       list(
 
         # consistency to tcpl
-        aic = aic,
-        fit = fit,
+        aic = cc2_aic,
+        fit = ifelse(!is.na(hillcoef), 1L, cc2_fit),
         modl = "cc2",
 
         # original output
         cc2 = as.numeric(v[2]),
-        #emax = as.numeric(v[3]),
         gw = hillcoef,
         tp = as.numeric(v[5]),
         bt = as.numeric(v[12]),
         ga = as.numeric(v[6]),
-        masks = masks,
-        nMasks = as.numeric(nmasks),
+        masks = cc2_masks,
+        nMasks = ifelse(!is.na(hillcoef), as.numeric(v[8]), cc2_nmasks),
         pvalue = as.numeric(v[10]),
         r2 = as.numeric(v[11])
 
